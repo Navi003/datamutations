15beb644f5103055412e72bf815d244be60daacb
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-10-22 19:42:13 +0200

    Uploading Image - Like button wokring

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,32 +1,17 @@
-import FromSubmit from "@/components/FromSubmit";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
+"use client";
 
+import FromSubmit from "@/components/FromSubmit";
+import { useFormState } from "react-dom";
+import { createPost } from "@/lib/actions";
 // adding Image
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    "use server";
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-
-    let errors = [];
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/feed");
-  }
+  const [state, formAction] = useFormState(createPost, {});
 
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
@@ -47,6 +32,10 @@ export default function NewPostPage() {
         <p className="form-actions">
           <FromSubmit />
         </p>
+
+        {/* {state?.errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))} */}
       </form>
     </>
   );
